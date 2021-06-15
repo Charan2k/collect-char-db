dcf451d1943587db728fe47795675c8e2937b513
Charan Vaddepally <charan316@Charans-BacMook-Air.local>
2021-06-15 23:50:21 +0530

    reformatted html

--- cgi-bin/image-upload.py
+++ cgi-bin/image-upload.py
@@ -1,17 +1,12 @@
-#!/opt/homebrew/bin/python3
+#!/usr/bin/python3
 import cgi, os
-print("Content-Type: text/html")
-print("")
+print("Content-type: text/html")
+print()
 
 form = cgi.FieldStorage()
 fileitem = form['filename']
-  
-# check if the file has been uploaded
-if fileitem.filename:
-    # strip the leading path from the file name
-    fn = os.path.basename(fileitem.filename)
-      
-   # open read and write the file into the server
-    open(fn, 'wb').write(fileitem.file.read())
 
+img_data = fileitem.file.read()
+fn = os.path.basename(fileitem.filename)      
+open(fn,'wb').write(img_data)
 print("upload successfull")
